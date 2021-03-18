796746dbc4803442019893cf11d3541be457e528
ajay jairath <gjairath@purdue.edu>
2021-03-18 00:07:39 -0400

    Better UI

--- IP/ip_main.py
+++ IP/ip_main.py
@@ -5,10 +5,33 @@ Created on Wed Mar 17 21:58:35 2021
 @author: garvit
 """
 
+from project import Project
+
 # Existing projects -> Big Tasks [Quaterly Review] -> Tasks [Do something] -> People
 
 # An array of existing projects.
 existing_projects = []
 
 if __name__ == "__main__":
-    print ("Hi!")
+    print ("Hi!\n")
+    
+    
+    new_project = Project()
+    
+    print ("-" * 20)
+    print(new_project.name)
+    print ("-" * 20)
+    print(new_project.is_active, "[Is Active?]")    
+    print ("-" * 20)
+    
+    print ("\n\nSub-Tasks:")
+    
+    print(new_project.sub_tasks[0].name)
+    print ("-" * 20)
+    print(new_project.sub_tasks[0].eta, "Minutes (Time Left)")
+    print ("-" * 20)
+    print(new_project.sub_tasks[0].finish_date)
+    print ("-" * 20)
+    print(new_project.sub_tasks[0].members)
+
+    
--- IP/project.py
+++ IP/project.py
@@ -5,10 +5,30 @@ Created on Wed Mar 17 23:01:37 2021
 @author: garvi
 """
 
+from sublist import SubList
 
 class Project:
-    # A class that holds a new project instance.
+    # A class that holds a new project instance. 
+    # A project object contains subtasks which hold "to-do lists" with whatever people.
     
     def __init__(self):
+        # A new class is made, provide the user with a basic template.
+        
+        self.name = "Untitled_Project"
         self.is_active = True
         
+        
+        empty_sub_task = self.make_sub_list()
+        self.sub_tasks = []
+        self.sub_tasks.append(empty_sub_task)
+        
+        self.num_sub_tasks = len(self.sub_tasks)
+        
+    
+    def make_sub_list(self):
+        # An object containing sublists
+        new_sub_list = SubList()
+        
+        return new_sub_list
+        
+    
